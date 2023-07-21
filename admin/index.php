e89713253689ad1d31c520712e9033cddac66e77
Milton Chithenga <miltonachithenga@gmail.com>
2023-07-21 16:06:43 +0200

    888

--- admin/index.php
+++ admin/index.php
@@ -21,7 +21,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home</a>
+                        <a class="nav-link" href="../index.php">Home</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="functionsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
